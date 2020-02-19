295edd7629441445d06f19b245c72fb15855d8bc
zhucq38 <60735670+zhucq38@users.noreply.github.com>
2020-02-19 16:28:02 +0800

    Update and rename config.py to monitor.py

--- config.py
+++
@@ -1,9 +0,0 @@
-#coding:utf-8
-import os 
-import sys
-import requests 
-
-# 招商银行 招邮通
-# URL = http://47.104.63.107:8080/fl/login
-# user = demo
-# password = demo
---
+++ monitor.py
@@ -0,0 +1,15 @@
+#coding:utf-8
+import os 
+import sys
+import requests 
+
+# 招商银行 招邮通
+URL = http://47.104.63.107:8080/fl/login
+#user = test
+#password = password
+data = {"userName":"test","password":"Z5/f9T0aUupM0bOhVdXjcVPDxM3o0VSmFonMQVgmdZXwd8kCVhkxSUnDfVQlxf3D4Pgo7ed5bB5MJzJ6Muu+64EgkqGo6aB2R62Yg3f4bQLElOqBd2O+A/+QjmCNuRMOI/ZNdKy9wETQO42Qb0EXAWsuLDItQpXMkQSWUt0A7LM=","companyID":"CUSTOMER"}
+req = requests.post(url,data=data,timeout=10)
+if "sendmsg" in req.content:
+    print("Success!")
+else:
+    print("Fail!")
